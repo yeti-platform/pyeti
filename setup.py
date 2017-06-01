b1fdabefa82ae3c9974bb102680b3ab33072e428
Thomas Chopitea <tomchop@gmail.com>
2017-06-02 00:05:25 +0200

    Changed some metadata

--- setup.py
+++ setup.py
@@ -4,39 +4,27 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-
 setup(name='pyeti',
       version="0.0.1",
       description='Python bindings for Yeti\'s API',
       long_description=readme(),
       classifiers=[
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Topic :: Threat Intelligence Plattform',
+        'Topic :: Threat Intelligence Platform',
       ],
       keywords='yeti threat intel api',
       url='https://github.com/yeti-platform/pyeti',
-      # author='To be defined',
-      # author_email='none@example.com',
-      license='BSD',
+      author='Yeti core developers',
+      license='Apache',
       packages=['pyeti'],
       install_requires=[
           'requests',
           'logging',
       ],
-      # As soon there are tests defined
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3'],
-      # Scripts we provide
-      scripts=[
-          'scripts/yeti_test_add_observable'
-      ],
-      #
-      # Future hint for console scripts
-      #entry_points={
-      #    'console_scripts': ['funniest-joke=funniest.command_line:main'],
-      #},
       include_package_data=True,
       zip_safe=False)
